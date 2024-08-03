7e132b730b9eb1faadc4605a9883ac5ae3a2815c
Chunwaski <tytaulananda@gmail.com>
2024-08-03 18:53:04 +0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -39,29 +39,3 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/fireba
                 document.getElementById('navbar').innerHTML = data;
             });
 
-            // Navigation functionality
-            const nav = document.querySelector(".nav"),
-                searchIcon = document.querySelector("#searchIcon"),
-                navOpenBtn = document.querySelector(".navOpenBtn"),
-                navCloseBtn = document.querySelector(".navCloseBtn");
-
-            searchIcon.addEventListener("click", () => {
-                nav.classList.toggle("openSearch");
-                nav.classList.remove("openNav");
-                if (nav.classList.contains("openSearch")) {
-                    searchIcon.classList.replace("uil-search", "uil-times");
-                } else {
-                    searchIcon.classList.replace("uil-times", "uil-search");
-                }
-            });
-
-            navOpenBtn.addEventListener("click", () => {
-                nav.classList.add("openNav");
-                nav.classList.remove("openSearch");
-                searchIcon.classList.replace("uil-times", "uil-search");
-            });
-
-            navCloseBtn.addEventListener("click", () => {
-                nav.classList.remove("openNav");
-            });
-        });
